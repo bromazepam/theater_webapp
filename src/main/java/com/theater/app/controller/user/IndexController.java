83889451fc18944422860b37b04bd85a4a1c89e4
bromazepam <52470950+bromazepam@users.noreply.github.com>
2020-06-17 12:27:57 +0200

    refactoring

--- src/main/java/com/theater/app/controller/user/IndexController.java
+++ src/main/java/com/theater/app/controller/user/IndexController.java
@@ -23,7 +23,7 @@ public class IndexController {
 
     @RequestMapping("/")
     public String index(){
-        return "redirect:/homescreen";
+        return "user/homescreen";
     }
 
     @RequestMapping("/login")
