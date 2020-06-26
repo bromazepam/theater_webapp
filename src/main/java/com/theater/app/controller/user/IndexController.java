f441a4d0580e9dc7447a747c9f45aad3a4618640
bromazepam <52470950+bromazepam@users.noreply.github.com>
2020-06-26 15:28:17 +0200

    [NF]
    new user

--- src/main/java/com/theater/app/controller/user/IndexController.java
+++ src/main/java/com/theater/app/controller/user/IndexController.java
@@ -2,24 +2,45 @@ package com.theater.app.controller.user;
 
 import com.theater.app.domain.Play;
 import com.theater.app.domain.Repertoire;
+import com.theater.app.domain.User;
+import com.theater.app.domain.security.Role;
+import com.theater.app.domain.security.UserRole;
 import com.theater.app.service.PlayService;
 import com.theater.app.service.RepertoireService;
+import com.theater.app.service.UserService;
+import com.theater.app.utility.MailConstructor;
+import com.theater.app.utility.SecurityUtility;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 @Controller
 public class IndexController {
 
     private final PlayService playService;
     private final RepertoireService repertoireService;
+    private final UserService userService;
+    private final MailConstructor mailConstructor;
+    private final JavaMailSender mailSender;
 
-    public IndexController(PlayService playService, RepertoireService repertoireService) {
+    public IndexController(PlayService playService, RepertoireService repertoireService, UserService userService,
+                           MailConstructor mailConstructor, JavaMailSender mailSender) {
         this.playService = playService;
         this.repertoireService = repertoireService;
+        this.userService = userService;
+        this.mailConstructor = mailConstructor;
+        this.mailSender = mailSender;
     }
 
     @RequestMapping("/")
@@ -42,6 +63,58 @@ public class IndexController {
         return "user/registration";
     }
 
+    @PostMapping("/newUser")
+    public String newUserPost(HttpServletRequest request,
+                              @ModelAttribute("email") String userEmail,
+                              @ModelAttribute("username") String username,
+                              Model model) throws Exception {
+
+        model.addAttribute("email", userEmail);
+        model.addAttribute("username", username);
+
+        if(userService.findByUsername(username) != null){
+            model.addAttribute("usernameExists", true);
+
+            return "user/registration";
+        }
+        if(userService.findByEmail(userEmail) != null){
+            model.addAttribute("emailExists", true);
+
+            return "user/registration";
+        }
+
+        User user = new User();
+        user.setUsername(username);
+        user.setEmail(userEmail);
+
+        String password = SecurityUtility.randomPassword();
+
+        String encryptedPassword = SecurityUtility.passwordEncoder().encode(password);
+        user.setPassword(encryptedPassword);
+
+        Role role = new Role();
+        role.setRoleId(1);
+        role.setName("ROLE_USER");
+        Set<UserRole> userRoles = new HashSet<>();
+        userRoles.add(new UserRole(user, role));
+        userService.createUser(user, userRoles);
+
+        String token = UUID.randomUUID().toString();
+        userService.createPasswordResetTokenForUser(user, token);
+
+        String appUrl = "http://" + request.getServerName()+":"+request.getServerPort()+request.getContextPath();
+
+        SimpleMailMessage email = mailConstructor.constructResetTokenEmail(appUrl, request.getLocale(), token, user, password);
+
+        mailSender.send(email);
+
+        model.addAttribute("emailSent", true);
+        //TODO
+//        model.addAttribute("orderList", user.getOrderList());
+
+        return "user/myAccount";
+
+    }
 
     @RequestMapping("/plays")
     public String plays(Model model) {
