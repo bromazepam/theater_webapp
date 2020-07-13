cf45e514143438d228d70862134c018535a65fea
bromazepam <52470950+bromazepam@users.noreply.github.com>
2020-07-13 23:08:46 +0200

    Update CartItemServiceImpl.java
    added method logic

--- src/main/java/com/theater/app/service/impl/CartItemServiceImpl.java
+++ src/main/java/com/theater/app/service/impl/CartItemServiceImpl.java
@@ -7,6 +7,7 @@ import com.theater.app.service.CartItemService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CartItemServiceImpl implements CartItemService {
@@ -25,31 +26,61 @@ public class CartItemServiceImpl implements CartItemService {
 
     @Override
     public CartItem updateCartItem(CartItem cartItem) {
-        return null;
+        int total = cartItem.getRepertoire().getPrice() * cartItem.getQty();
+        cartItem.setSubtotal(total);
+
+        cartItemRepository.save(cartItem);
+        return cartItem;
     }
 
     @Override
     public CartItem addRepertoireToCartItem(Repertoire repertoire, User user, int qty) {
-        return null;
+        List<CartItem> cartItemList = findByShoppingCart(user.getShoppingCart());
+
+        for(CartItem cartItem: cartItemList){
+            if (repertoire.getId() == cartItem.getRepertoire().getId()) {
+                cartItem.setQty(cartItem.getQty()+qty);
+                cartItem.setSubtotal(repertoire.getPrice() * qty);
+                cartItemRepository.save(cartItem);
+                return cartItem;
+            }
+        }
+
+        CartItem cartItem = new CartItem();
+        cartItem.setShoppingCart(user.getShoppingCart());
+        cartItem.setRepertoire(repertoire);
+
+        cartItem.setQty(qty);
+        cartItem.setSubtotal(repertoire.getPrice() * qty);
+        cartItem = cartItemRepository.save(cartItem);
+
+        RepertoireToCartItem repertoireToCartItem = new RepertoireToCartItem();
+        repertoireToCartItem.setRepertoire(repertoire);
+        repertoireToCartItem.setCartItem(cartItem);
+        repertoireToCartItemRepository.save(repertoireToCartItem);
+
+        return cartItem;
     }
 
     @Override
     public void removeCartItem(CartItem cartItem) {
+        repertoireToCartItemRepository.deleteByCartItem(cartItem);
+        cartItemRepository.delete(cartItem);
 
     }
 
     @Override
-    public CartItem findById(Long id) {
-        return null;
+    public Optional<CartItem> findById(Long id) {
+        return cartItemRepository.findById(id);
     }
 
     @Override
     public CartItem save(CartItem cartItem) {
-        return null;
+        return cartItemRepository.save(cartItem);
     }
 
     @Override
     public List<CartItem> findByOrder(Order order) {
-        return null;
+        return cartItemRepository.findByOrder(order);
     }
 }
