c4e8d97aa41b272b911e8672eabda45b370c6f8c
bromazepam <52470950+bromazepam@users.noreply.github.com>
2020-07-28 23:30:58 +0200

    [BF]
    fixed generating report

--- src/main/java/com/theater/app/service/impl/ReportServiceImpl.java
+++ src/main/java/com/theater/app/service/impl/ReportServiceImpl.java
@@ -1,7 +1,11 @@
 package com.theater.app.service.impl;
 
+import com.theater.app.domain.Play;
 import com.theater.app.domain.Repertoire;
+import com.theater.app.domain.Stage;
+import com.theater.app.repository.PlayRepository;
 import com.theater.app.repository.RepertoireRepository;
+import com.theater.app.repository.StageRepository;
 import com.theater.app.service.ReportService;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
@@ -18,9 +22,14 @@ import java.util.Map;
 public class ReportServiceImpl implements ReportService {
 
     private final RepertoireRepository repertoireRepository;
+    private final PlayRepository playRepository;
+    private final StageRepository stageRepository;
 
-    public ReportServiceImpl(RepertoireRepository repertoireRepository) {
+    public ReportServiceImpl(RepertoireRepository repertoireRepository, PlayRepository playRepository,
+                             StageRepository stageRepository) {
         this.repertoireRepository = repertoireRepository;
+        this.playRepository = playRepository;
+        this.stageRepository = stageRepository;
     }
 
     @Override
@@ -28,11 +37,12 @@ public class ReportServiceImpl implements ReportService {
         String path = "C:\\Users\\David\\Desktop";
         List<Repertoire> repertoireList = (List<Repertoire>) repertoireRepository.findAll();
         //load file and compile it
-        File file = ResourceUtils.getFile("classpath:repertoire.jrxml");
+        File file = ResourceUtils.getFile("src/main/resources/reports/repertoire.jrxml");
         JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
         JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(repertoireList);
         Map<String, Object> parameters = new HashMap<>();
         parameters.put("createdBy", "David");
+
         JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
         if (reportFormat.equalsIgnoreCase("html")) {
             JasperExportManager.exportReportToHtmlFile(jasperPrint, path + "\\repertoar.html");
@@ -40,6 +50,6 @@ public class ReportServiceImpl implements ReportService {
         if (reportFormat.equalsIgnoreCase("pdf")) {
             JasperExportManager.exportReportToPdfFile(jasperPrint, path + "\\repertoar.pdf");
         }
-        return "report generated in path :  " + path;
+        return "admin/reports";
     }
 }
