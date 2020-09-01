1a52eda512981ba74bce5037bb3de510e86d13ff
bromazepam <52470950+bromazepam@users.noreply.github.com>
2020-09-01 23:30:11 +0200

    refactoring
    query works but there is a bug with parsing data

--- src/main/java/com/theater/app/repository/OrderRepository.java
+++ src/main/java/com/theater/app/repository/OrderRepository.java
@@ -5,9 +5,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface OrderRepository extends CrudRepository<Order, Long> {
-    @Query(value = "select order_date, SUM(order_total) as total from user_order group by month(order_date)", nativeQuery = true)
-    Optional<List<Object[]>> getAggregates();
+    @Query(value = "select order_date as profitMonth, SUM(order_total) as profitTotal from user_order group by month(order_date)", nativeQuery = true)
+    List<Order> getAggregates();
 }
