bd14873fa2866760754279c97aab26a28e081a25
bromazepam <52470950+bromazepam@users.noreply.github.com>
2020-09-15 15:40:28 +0200

    updating createOrder() method

--- src/main/java/com/theater/app/service/impl/OrderServiceImpl.java
+++ src/main/java/com/theater/app/service/impl/OrderServiceImpl.java
@@ -2,11 +2,12 @@ package com.theater.app.service.impl;
 
 import com.theater.app.domain.*;
 import com.theater.app.exceptions.NotFoundException;
-import com.theater.app.repository.OrderRepository;
+import com.theater.app.repository.*;
 import com.theater.app.service.CartItemService;
 import com.theater.app.service.OrderService;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Optional;
@@ -16,10 +17,15 @@ public class OrderServiceImpl implements OrderService {
 
     private final CartItemService cartItemService;
     private final OrderRepository orderRepository;
+    private final RepertoireRepository repertoireRepository;
+    private final UserRepository userRepository;
 
-    public OrderServiceImpl(CartItemService cartItemService, OrderRepository orderRepository) {
+    public OrderServiceImpl(CartItemService cartItemService, OrderRepository orderRepository,
+                            RepertoireRepository repertoireRepository, UserRepository userRepository) {
         this.cartItemService = cartItemService;
         this.orderRepository = orderRepository;
+        this.repertoireRepository = repertoireRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -30,19 +36,21 @@ public class OrderServiceImpl implements OrderService {
 
         List<CartItem> cartItemList = cartItemService.findByShoppingCart(shoppingCart);
 
-        for(CartItem cartItem: cartItemList){
+        for (CartItem cartItem : cartItemList) {
             Repertoire repertoire = cartItem.getRepertoire();
-            cartItem.setOrder(order);
-            repertoire.setAvailableSeats(repertoire.getAvailableSeats()-cartItem.getQty());
+            repertoire.setAvailableSeats(repertoire.getAvailableSeats() - cartItem.getQty());
+            repertoireRepository.save(repertoire);
         }
 
         order.setCartItemList(cartItemList);
         order.setOrderDate(Calendar.getInstance().getTime());
         order.setOrderTotal(shoppingCart.getGrandTotal());
-        payment.setOrder(order);
-        order.setUser(user);
         order = orderRepository.save(order);
 
+        List<Order> orderList = userRepository.findByUsername(user.getUsername()).getOrderList();
+        orderList.add(order);
+        user.setOrderList(orderList);
+        userRepository.save(user);
         return order;
     }
 
