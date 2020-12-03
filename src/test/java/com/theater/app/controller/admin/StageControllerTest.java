9726eb8f6925a1afccb416f9dc6d4fd5ab601342
bromazepam <52470950+bromazepam@users.noreply.github.com>
2020-12-03 23:48:02 +0100

    Update StageControllerTest.java

--- src/test/java/com/theater/app/controller/admin/StageControllerTest.java
+++ src/test/java/com/theater/app/controller/admin/StageControllerTest.java
@@ -3,6 +3,7 @@ package com.theater.app.controller.admin;
 import com.theater.app.domain.Stage;
 import com.theater.app.service.SeatService;
 import com.theater.app.service.StageService;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,8 +15,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
-import static org.mockito.Mockito.reset;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -53,22 +58,53 @@ class StageControllerTest {
     }
 
     @Test
-    void addStagePost() {
+    void addStagePost() throws Exception {
+        mockMvc.perform(post("/addStage")
+                .param("id","1")
+                .param("name","sala")
+                .param("capacity","200"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:stageList"));
     }
 
     @Test
-    void allStages() {
+    void allStages() throws Exception {
+        given(stageService.findAll()).willReturn(Lists.newArrayList(new Stage(), new Stage()));
+
+        mockMvc.perform(get("/stageList"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("admin/stage/stageList"));
     }
 
     @Test
-    void updateStage() {
+    void updateStage() throws Exception {
+        Stage stage = new Stage();
+        model.addAttribute("stage", stage);
+        given(stageService.findById(any())).willReturn(stage);
+
+        mockMvc.perform(get("/updateStage/" + stage.getId()+"/"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("stage"))
+                .andExpect(view().name("admin/stage/updateStage"));
     }
 
     @Test
-    void updateStagePost() {
+    void updateStagePost() throws Exception {
+        mockMvc.perform(post("/addStage")
+                .param("name","sala")
+                .param("capacity","200"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:stageList"));
     }
 
     @Test
-    void remove() {
+    void remove() throws Exception {
+        doNothing().when(stageService).remove(anyString());
+        List<Stage> stageList = stageService.findAll();
+        model.addAttribute("stageList", stageList);
+        this.mockMvc.perform(get("/removeStage/{id}/","1"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("stageList"))
+                .andExpect(view().name("admin/stage/stageList"));
     }
 }
