e4cb619170fc693f4661740da15891cf5c25fcdd
bromazepam <52470950+bromazepam@users.noreply.github.com>
2020-12-10 18:23:37 +0100

    Update RepertoireControllerTest.java

--- src/test/java/com/theater/app/controller/admin/RepertoireControllerTest.java
+++ src/test/java/com/theater/app/controller/admin/RepertoireControllerTest.java
@@ -1,31 +1,36 @@
 package com.theater.app.controller.admin;
 
 import com.theater.app.domain.Repertoire;
+import com.theater.app.domain.Stage;
 import com.theater.app.service.PlayService;
 import com.theater.app.service.RepertoireService;
 import com.theater.app.service.StageService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class RepertoireControllerTest {
 
     @InjectMocks
@@ -60,6 +65,20 @@ class RepertoireControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(model().attributeExists("repertoire","plays","stageList"))
                 .andExpect(view().name("admin/repertoire/addRepertoire"));
+    }
+
+    @Disabled("ima neki problem")
+    @Test
+    void addRepertoirePost() throws Exception {
+        Repertoire repertoire = mock(Repertoire.class);
+        Stage stage = mock(Stage.class);
+        doNothing().when(repertoire).setAvailableSeats(anyInt());
+        doNothing().when(repertoire).setStage(stage);
+        given(repertoireService.save(repertoire)).willReturn(repertoire);
+        mockMvc.perform(post("/addRepertoire"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:repertoire"));
+
     }
 
     @Test
