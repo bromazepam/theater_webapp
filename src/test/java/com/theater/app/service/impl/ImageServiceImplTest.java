75ca731fff44543aa9c4a48be4f59d305121570d
bromazepam <52470950+bromazepam@users.noreply.github.com>
2020-12-11 22:28:28 +0100

    Create ImageServiceImplTest.java

---
+++ src/test/java/com/theater/app/service/impl/ImageServiceImplTest.java
@@ -0,0 +1,48 @@
+package com.theater.app.service.impl;
+
+import com.theater.app.domain.Play;
+import com.theater.app.repository.PlayRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyByte;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+
+class ImageServiceImplTest {
+
+    @InjectMocks
+    ImageServiceImpl imageService;
+    @Mock
+    PlayRepository playRepository;
+
+    @Test
+    void saveImageFile() {
+        Play play = new Play();
+        String fileName = "test.txt";
+        MultipartFile multipartFile = new MockMultipartFile("user-file",fileName,
+                "text/plain", "test data".getBytes());
+        given(playRepository.findById(any())).willReturn(java.util.Optional.of(play));
+        byte[] bytes = new byte[anyByte()];
+
+        //when
+        play.setPlayImage(bytes);
+        imageService.saveImageFile(play.getId(),multipartFile);
+
+        //then
+        then(playRepository).should().save(play);
+
+
+    }
+}
