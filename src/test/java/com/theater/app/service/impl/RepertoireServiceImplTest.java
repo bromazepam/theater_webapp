a62879b62a04f0a317a2907032d94a457523582b
bromazepam <52470950+bromazepam@users.noreply.github.com>
2020-12-17 14:46:33 +0100

    [BF]
    findByDate, findByPresentOrFutureDate - passed
    - added  'Date date = mock(Date.class);'

--- src/test/java/com/theater/app/service/impl/RepertoireServiceImplTest.java
+++ src/test/java/com/theater/app/service/impl/RepertoireServiceImplTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
@@ -21,9 +20,9 @@ import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -77,11 +76,12 @@ class RepertoireServiceImplTest {
     @Test
     void findByDate() {
         //given
+        Date date = mock(Date.class);
         List<Repertoire> repertoireList = new ArrayList<>();
         given(repertoireRepository.findByProjectionDate(any(Date.class))).willReturn(repertoireList);
 
         //when
-        Collection<Repertoire> returnedRepertoire = repertoireService.findByDate(any(Date.class));
+        Collection<Repertoire> returnedRepertoire = repertoireService.findByDate(date);
 
         //then
         then(repertoireRepository).should().findByProjectionDate(any(Date.class));
@@ -112,14 +112,15 @@ class RepertoireServiceImplTest {
     @Test
     void findByPresentOrFutureDate() {
         //given
+        Date date = mock(Date.class);
         List<Repertoire> repertoireList = new ArrayList<>();
-        given(repertoireRepository.findByProjectionDateGreaterThanEqual(any())).willReturn(repertoireList);
+        given(repertoireRepository.findByProjectionDateGreaterThanEqual(any(Date.class))).willReturn(repertoireList);
 
         //when
-        Collection<Repertoire> returnedRepertoire = repertoireService.findByPresentOrFutureDate(any(Date.class));
+        Collection<Repertoire> returnedRepertoire = repertoireService.findByPresentOrFutureDate(date);
 
         //then
-        then(repertoireRepository).should().findByProjectionDateGreaterThanEqual(any());
+        then(repertoireRepository).should().findByProjectionDateGreaterThanEqual(any(Date.class));
         assertThat(returnedRepertoire).isNotNull();
     }
 }
