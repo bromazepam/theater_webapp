87668b9b1f3ae45a0d389008ecd3db7bc869e149
bromazepam <52470950+bromazepam@users.noreply.github.com>
2021-01-05 15:48:37 +0100

    Create UserSecurityServiceTest.java

---
+++ src/test/java/com/theater/app/service/impl/UserSecurityServiceTest.java
@@ -0,0 +1,43 @@
+package com.theater.app.service.impl;
+
+import com.theater.app.domain.User;
+import com.theater.app.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+
+@ExtendWith(MockitoExtension.class)
+class UserSecurityServiceTest {
+
+    @InjectMocks
+    UserSecurityService userSecurityService;
+    @Mock
+    UserRepository userRepository;
+
+    @Test
+    void loadUserByUsername() {
+        given(userRepository.findByUsername(anyString())).willReturn(new User());
+
+        User user = userRepository.findByUsername(anyString());
+
+        then(userRepository).should().findByUsername(anyString());
+        assertThat(user).isNotNull();
+
+    }
+
+    @Test
+    void loadUserByUsernameNotFound() {
+        User user = userRepository.findByUsername("2");
+
+        assertThat(user).isNull();
+
+    }
+}
