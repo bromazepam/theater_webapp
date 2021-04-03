d02b25de4729346bcb52adb5b79cc34efc8e4d47
bromazepam <graor95@gmail.com>
2021-04-03 14:48:02 +0200

    domain tests
    user and userPayment tests

---
+++ src/test/java/com/theater/app/domain/UserPaymentTest.java
@@ -0,0 +1,28 @@
+package com.theater.app.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+class UserPaymentTest {
+
+    @Test
+    void groupedAssertions() {
+        User user = mock(User.class);
+        UserPayment userPayment = new UserPayment("1", "type", "name", "number", 10,
+                2022, 222, "holderName", true, user);
+
+        assertAll("test play",
+                () -> assertEquals(userPayment.getId(), "1", "userPayment id failed"),
+                () -> assertEquals(userPayment.getType(), "type", "userPayment type failed"),
+                () -> assertEquals(userPayment.getCardName(), "name", "userPayment cardName failed"),
+                () -> assertEquals(userPayment.getCardNumber(), "number", "userPayment cardNumber failed"),
+                () -> assertEquals(userPayment.getExpiryMonth(), 10, "userPayment expiryMonth failed"),
+                () -> assertEquals(userPayment.getExpiryYear(), 2022, "userPayment expiryYear failed"),
+                () -> assertEquals(userPayment.getHolderName(), "holderName", "userPayment holderName failed"),
+                () -> assertTrue(userPayment.isDefaultPayment(), "userPayment defaultPayment failed"),
+                () -> assertEquals(userPayment.getUser(), user, "user failed"));
+    }
+
+}
---
+++ src/test/java/com/theater/app/domain/UserTest.java
@@ -0,0 +1,36 @@
+package com.theater.app.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+class UserTest {
+
+    @Test
+    void groupedAssertions() {
+        ShoppingCart shoppingCart = mock(ShoppingCart.class);
+        ArrayList arrayList = mock(ArrayList.class);
+        Set set = mock(Set.class);
+        User user = new User("1", "username", "password", "firstName", "lastName",
+                "email", "phone", true, shoppingCart, arrayList, arrayList, set);
+
+        assertAll("test play",
+                () -> assertEquals(user.getId(), "1"),
+                () -> assertEquals(user.getUsername(), "username"),
+                () -> assertEquals(user.getPassword(), "password"),
+                () -> assertEquals(user.getFirstName(), "firstName"),
+                () -> assertEquals(user.getLastName(), "lastName"),
+                () -> assertEquals(user.getEmail(), "email"),
+                () -> assertEquals(user.getPhone(), "phone"),
+                () -> assertTrue(user.isEnabled(), "user enabled failed"),
+                () -> assertEquals(user.getShoppingCart(), shoppingCart),
+                () -> assertEquals(user.getUserPaymentList(), arrayList),
+                () -> assertEquals(user.getOrderList(), arrayList),
+                () -> assertEquals(user.getUserRoles(), set));
+    }
+
+}
