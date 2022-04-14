da5d1e593a4a0c7761abf797fbee12806c49f0c4
bromazepam <graor95@gmail.com>
2022-04-14 19:37:24 +0200

    Role Repository test

--- src/main/java/com/theater/app/repository/RoleRepository.java
+++ src/main/java/com/theater/app/repository/RoleRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface RoleRepository extends CrudRepository<Role, String> {
-	Optional<Role> findByName(String name);
+	Role findByName(String name);
 }
--- src/test/java/com/theater/app/repository/RoleRepositoryTest.java
+++ src/test/java/com/theater/app/repository/RoleRepositoryTest.java
@@ -4,35 +4,34 @@ import com.theater.app.domain.security.Role;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 
-import java.util.Optional;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
+@DataMongoTest
 class RoleRepositoryTest {
 
     @Autowired
     private RoleRepository roleRepository;
 
     @AfterEach
-    void tearDown(){
-        roleRepository.deleteById("10");
+    void tearDown() {
+        roleRepository.deleteById("1");
     }
 
     @Test
     void findByName() {
         //given
-        Role role = new Role("10","superAdmin");
+        Role role = Role.builder()
+                .name("superAdmin")
+                .roleId("1").build();
         roleRepository.save(role);
         //when
-        Optional<Role> expected = roleRepository.findByName("superAdmin");
+        Role expected = roleRepository.findByName("superAdmin");
         //then
-        assertThat(expected.get().getName()).isEqualTo(role.getName());
+        assertThat(expected.getName()).isEqualTo(role.getName());
     }
 }
